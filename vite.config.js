fb7e69747961bd427bedb9c3ab34d0da1ceff625
jamoragar <javier.moragarojas@gmail.com>
2023-12-26 19:21:42 +0100

    fix[.gitignore]: Adding dist folder

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/dante_landing/', // this is for GithubPages
   plugins: [react()]
 })
